0a08c1dae432c47b4af4abe6f3b53c4301338066
Maksym Ząbroń <mzabron@student.agh.edu.pl>
2026-07-29 17:53:34 +0200

    chore: setup workflow (prettier, CI, docker-compose, openapi)

--- backend/src/PrepChess.Api/Program.cs
+++ backend/src/PrepChess.Api/Program.cs
@@ -1,5 +1,6 @@
-var builder = WebApplication.CreateBuilder(args);
+using Scalar.AspNetCore;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -10,6 +11,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
